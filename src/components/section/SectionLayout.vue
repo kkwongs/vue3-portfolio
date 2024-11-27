bfc6b17d1c73b63951197c84c9133fde5e97a185
Gihyun Gwon <kihyun5741@gmail.com>
2024-11-27 11:29:25 +0900

    feat: create section layout

---
+++ src/components/section/SectionLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="section-wrapper">
+    <div class="section-container">
+      <h2 class="title">{{ title }}</h2>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.section-wrapper {
+  .section-container {
+    display: grid;
+    gap: 60px;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 80px 40px;
+    .title {
+      font-size: 3rem;
+      font-weight: 700;
+      text-align: center;
+    }
+  }
+}
+</style>
