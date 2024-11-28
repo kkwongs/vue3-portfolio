93533f0fef3e2f10a8c77cbba3dfb11df8e75fb1
Gihyun Gwon <kihyun5741@gmail.com>
2024-11-28 17:41:14 +0900

    feat: add reset.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './routes'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./routes";
 
+import "@/assets/scss/main.scss";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app')
+createApp(App).use(store).use(router).mount("#app");
