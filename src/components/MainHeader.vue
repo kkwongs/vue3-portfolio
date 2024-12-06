4cf84fdcf074358516e5428062fbd6d7fbc1062b
Gihyun Gwon <kihyun5741@gmail.com>
2024-12-06 22:49:21 +0900

    feat: adjust response web in Header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,18 @@
 <template>
   <header class="header">
     <div class="container">
-      <div>
+      <div class="logo-container">
         <router-link to="/">
           <Button size="extra-large" color="light">KKWONG'S Portfolio</Button>
         </router-link>
       </div>
-      <div>
-        <ul class="section-nav">
+
+      <div class="nav-container">
+        <ul
+          class="section-nav"
+          :class="{ 'section-nav--active': isShowNavigation }"
+          ref="refSectionNav"
+        >
           <Button
             v-for="(sectionName, sectionId) in sections"
             :key="sectionId"
@@ -18,6 +23,12 @@
             {{ sectionName }}
           </Button>
         </ul>
+        <button
+          class="section-nav-toggle__button"
+          @click.stop="isShowNavigation = !isShowNavigation"
+        >
+          <MenuIcon fillColor="#f8f9fa" />
+        </button>
       </div>
     </div>
   </header>
@@ -25,16 +36,30 @@
 
 <script>
 import Button from "@/components/BaseButton.vue";
+import MenuIcon from "vue-material-design-icons/Menu.vue";
+
 export default {
   name: "MainHeader",
-  components: { Button },
+  components: { Button, MenuIcon },
   data() {
     return {
       sections: {
         "about-me": "About me",
         projects: "Projects",
       },
+      isShowNavigation: false,
     };
+  },
+  watch: {
+    isShowNavigation(newValue) {
+      if (newValue) {
+        document.addEventListener("click", this.clickOutSideNav);
+        document.addEventListener("keydown", this.keydownEsc);
+      } else {
+        document.removeEventListener("click", this.clickOutSideNav);
+        document.removeEventListener("keydown", this.keydownEsc);
+      }
+    },
   },
   methods: {
     scrollToSection(sectionId) {
@@ -43,12 +68,30 @@ export default {
       const top = sectionOffset - headerHeight;
 
       window.scrollTo({ top, behavior: "smooth" });
+
+      this.isShowNavigation = false;
+    },
+
+    clickOutSideNav(e) {
+      const refSectionNav = this.$refs.refSectionNav;
+
+      if (refSectionNav && !refSectionNav.contains(e.target)) {
+        this.isShowNavigation = false;
+      }
+    },
+
+    keydownEsc(e) {
+      if (e.key === "Escape") {
+        this.isShowNavigation = false;
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/mixins";
+
 .header {
   position: sticky;
   top: 0;
@@ -58,10 +101,51 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 20px 40px;
-    .section-nav {
-      display: flex;
-      gap: 12px;
+    padding: 1.25rem 2.5rem;
+
+    $header-y-padding: 1rem;
+    @include media(medium) {
+      padding: $header-y-padding 1.25rem;
+    }
+    .nav-container {
+      .section-nav {
+        display: flex;
+        gap: 0.75rem;
+        @include media(medium) {
+          position: absolute;
+          top: 100%;
+          left: 0;
+          flex-direction: column;
+          gap: 0.25rem;
+          background-color: #222;
+          width: 100%;
+          max-height: 0;
+          padding: 0 1.25rem;
+          transition: max-height 0.3s ease-in-out;
+          overflow: hidden;
+          &--active {
+            max-height: 5.5rem;
+          }
+          > :last-child {
+            margin-bottom: $header-y-padding;
+          }
+        }
+      }
+
+      .section-nav-toggle__button {
+        display: none;
+        @include media(medium) {
+          display: inline-block;
+          aspect-ratio: 1;
+          background-color: inherit;
+          border: 0;
+          cursor: pointer;
+          &:hover {
+            background-color: rgba(#f8f9fa, 0.1);
+            border-radius: 0.125rem;
+          }
+        }
+      }
     }
   }
 }
